418232e461597ebbdfa792b98552763fc3057b2d
cmcp0 <cmcacerespalacio@gmail.com>
2014-10-27 11:15:37 -0500

    first commit

---
+++ akpt/index.js
@@ -0,0 +1,37 @@
+var elements = ["start","input","output","process","if"],
+	$bar = $("#contenido .slt ul"),
+	$ele;
+
+for (var i = 0; i < elements.length; i++) {
+	
+	elements[i];
+	$bar.append("<li></li>");
+	$("#contenido .slt ul li").eq(i).append(elements[i]).addClass("lel");
+	
+};
+
+$ele = $("#contenido .slt ul li");
+
+
+
+function newblock (types){
+
+	this.type = types;
+	switch (types){
+
+		case "start":
+			if( $(".canvas .start").length != 0 ){
+
+				$(".workspace p").append("There is already a Start object");
+
+			} else {
+				$(".canvas .box").append($("<p></p>").text(types).addClass("start"));
+				$(".workspace p").append("Object added");
+			}
+	}
+}
+
+
+
+$ele.eq(0).click(function(){ newblock("start");});
+
